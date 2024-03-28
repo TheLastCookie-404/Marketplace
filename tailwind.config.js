ecb5a036fd5a5f2ac8b695bafa5019c94b539d20
TheLastCookie-404 <se660745@gmail.com>
2024-03-28 10:28:46 +0500

    ThemeAdded

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        base: {
+          "primary": "#ffc700",
+          "secondary": "#d32727",
+          "accent": "#b52b2b",
+          "neutral": "#cfcfcf",
+          "base-100": "#ffffff",
+          "info": "#38bdf8",
+          "success": "#00e8aa",
+          "warning": "#fcd34d",
+          "error": "#ff6382",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
